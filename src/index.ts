b376c452871cb6f7c7d275b53181ff2b58a5728d
@yeison9 <55843595+TGYeison@users.noreply.github.com>
2023-04-22 07:42:20 -0500

    last details

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-
+import cors from 'cors';
 import routes from './router/index';
 
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
